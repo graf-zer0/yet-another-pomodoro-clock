9c07bc6ee916b49f3ae9ad961e691866e5bc2d78
graf-zer0 <graf0@protonmail.ch>
2018-11-19 13:22:04 +0100

    basic webpack config

---
+++ src/app.js
@@ -0,0 +1,2 @@
+import './styles/main.css'
+console.log(`webpack is working`)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+})
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/app.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test : /.css$/,
+        use : ['style-loader', 'css-loader']
+      },
+      {
+        test: /.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+}
