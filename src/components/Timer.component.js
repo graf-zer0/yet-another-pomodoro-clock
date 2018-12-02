978b5fa9cf881711f4659a008dbf8eab4d5489fa
graf-zer0 <graf0@protonmail.ch>
2018-12-02 19:20:08 +0100

    removed minutesLeft from state and added handleTime function to render format time properly

--- src/components/Timer.component.js
+++ src/components/Timer.component.js
@@ -4,29 +4,26 @@ export class Timer extends React.Component {
   constructor(props){
     super(props)
       this.state = {
-        minutesLimit : this.props.minutes - 1,
-        minutesLeft : this.minutesLimit,
-        secondsLeft : 59,
+
+        initialTime : this.props.minutes,
+        secondsLeft : 60 * this.props.minutes,
         status : 'fresh'
         }
         this.startTimer = this.startTimer.bind(this)
         this.resetTimer = this.resetTimer.bind(this)
         this.pauseTimer = this.pauseTimer.bind(this)
         this.renderStatus = this.renderStatus.bind(this)
+        this.handleTime = this.handleTime.bind(this)
+        console.log(this.state.secondsLeft)
+        console.log(this.state.minutesLeft)
       }
       startTimer(){
         this.setState({status : this.state.status = 'active'})
         timer = setInterval(_=> {
            if( this.state.secondsLeft > 0 ) this.setState({secondsLeft : this.state.secondsLeft - 1})
-           if( this.state.secondsLeft === 0 && this.state.minutesLeft > 0)
-            {
-              this.setState({
-                minutesLeft : minutesLeft - 1,
-                secondsLeft : secondsLeft = 59
-              })
-          }
-           if(this.state.secondsLeft === 0 && this.state.minutesLeft === 0)
+           if(this.state.secondsLeft === 0)
            {
+             this.setState({status : this.state.status = 'fresh'})
              clearInterval(timer)
            }
 
@@ -39,10 +36,20 @@ export class Timer extends React.Component {
       }
       resetTimer(){
         this.setState({
-          minutesLeft : this.state.minutesLimit,
-          secondsLeft : 59,
+          secondsLeft : this.state.initialTime * 60,
           status : this.state.status = 'fresh'
         })
+      }
+      handleTime(){
+        console.log('handle time')
+        let { secondsLeft } = this.state;
+        let minutes = Math.ceil(secondsLeft/60)
+        if(minutes < 10 ) minutes = '0' + minutes
+        if(secondsLeft < 10 ) secondsLeft = '0' + secondsLeft
+        if( secondsLeft % 60 === 0){
+          return `${ minutes } : 00`
+        }
+        else return `${ minutes - 1 } : ${secondsLeft}`
       }
       renderStatus(){
         switch(this.state.status) {
@@ -55,21 +62,21 @@ export class Timer extends React.Component {
         }
       }
       render(){
-        let { minutesLeft, secondsLeft, status} = this.state
+        let { handleTime, status} = this.state
         return (
           <div className="grid-wrapper">
-            <p className="g-r1-full flex-center">{minutesLeft} : {secondsLeft}</p>
+            <p className="timer-displayer g-r1-full flex-center">{this.handleTime()}</p>
             <p className="g-r2-full flex-center">{this.renderStatus()}</p>
             <button
-              className='g-r3-c1'
+              className='btn btn-success g-r3-c1'
               disabled={status === 'active' ? true : false} onClick={this.startTimer}>Start
             </button>
             <button
-              className='g-r3-c2'
+              className=' btn btn-pause g-r3-c2'
               disabled={status === 'paused' ? true : false} onClick={this.pauseTimer}>Pause
             </button>
             <button
-              className='g-r4-full'
+              className='btn btn-danger g-r4-full'
               disabled={status === 'active' ? true : false} onClick={this.resetTimer}>Reset
             </button>
           </div>
